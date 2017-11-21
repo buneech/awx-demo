fdc704015322894f771887ac7b065e1d7583bf0a
Bojan Bunic <bojan.bunic@src.si>
2017-11-21 11:51:51 +0100

    changed yml, added php site

---
+++ httpd/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Index page</title>
+  <style>
+   div {
+    height: 200px;
+    width: 400px;
+
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-top: -100px;
+    margin-left: -200px;
+   }
+  </style>
+ </head>
+ <body bgcolor="#81d4f7">
+ <?php '<div><span style="color: white; font-family: arial; font-weight: bold; font-size: 60px;">' echo gethostname(); '</span></div>' ?> 
+ </body>
+</html>
